9cdc833a5cf3d435e39129f7a7cd7881a34af4c4
wangkaiwd <1162486176@qq.com>
2020-09-25 17:30:43 +0800

    feat: loop generate all col class

--- src/components/grid/col.vue
+++ src/components/grid/col.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="go-col">
+  <div class="go-col" :class="`go-col-${span}`">
     <slot></slot>
   </div>
 </template>
@@ -7,6 +7,12 @@
 <script>
 export default {
   name: 'GoCol',
+  props: {
+    span: {
+      type: Number,
+      default: 24
+    }
+  },
   data () {
     return {};
   },
@@ -15,6 +21,11 @@ export default {
 
 <style lang="scss" scoped>
 .go-col {
-
+  // 循环生成所有的col
+  @for $i from 1 through 24 {
+    &-#{$i} {
+      width: calc(#{$i} / 24 * 100%);
+    }
+  }
 }
 </style>
--- src/components/grid/row.vue
+++ src/components/grid/row.vue
@@ -7,9 +7,27 @@
 <script>
 export default {
   name: 'GoRow',
+  props: {
+    gutter: {
+      type: Number
+    }
+  },
   data () {
     return {};
   },
+  mounted () {
+    this.checkChildren();
+  },
+  methods: {
+    checkChildren () {
+      this.$children.forEach((child) => {
+        const { name } = child;
+        if (name !== 'GoCol') {
+          console.log('Row child component must be Col');
+        }
+      });
+    }
+  }
 };
 </script>
 
