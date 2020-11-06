a65c4a0e3a092196f2bdfe82db1cba6c282fc7ff
wangkaiwd <1162486176@qq.com>
2020-11-06 18:16:05 +0800

    feat: get scroll container

--- src/components/infinite-scroll/index.js
+++ src/components/infinite-scroll/index.js
@@ -1,4 +1,10 @@
 const scope = 'GoInfiniteScroll';
+const defaultProps = {
+  disabled: false,
+  distance: 40,
+  immediate: true,
+  delay: 300
+};
 const getContainer = (el) => {
   let container = el;
   while (container) {
@@ -6,13 +12,34 @@ const getContainer = (el) => {
       return container;
     }
     const { overflowY, overflow } = getComputedStyle(container);
+    const regexp = /auto|scroll/;
+    if (regexp.test(overflow) || regexp.test(overflowY)) {
+      return container;
+    }
+    container = container.parentNode;
   }
 };
+const getOptions = (el) => {
+
+  // Object.entries(([key,val]) => {
+  //
+  // })
+};
+
+function handleScroll (e) {
+  // 1. 如果没有加载到指定高度，就会调用load方法进行加载
+
+}
+
 const install = (Vue) => {
   Vue.directive(scope, {
     name: scope,
-    bind (el) { // Only called once, when the directive is first bound to the element. This is where you can do one-time setup work
-
+    bind (el, binding) { // Only called once, when the directive is first bound to the element. This is where you can do one-time setup work
+      Vue.nextTick(() => {
+        const container = getContainer(el);
+        // const onScroll = handleScroll.bind(container);
+        container.addEventListener('scroll', handleScroll);
+      });
     }
   });
 };
